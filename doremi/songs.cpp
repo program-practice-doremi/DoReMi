7614403b2db65e1036bf44fda097f9788f24d242
CalvinXiaocao <calvinxiaocao@gmail.com>
2024-05-17 18:13:04 +0800

    finish music ocean part b

--- doremi/songs.cpp
+++ doremi/songs.cpp
@@ -75,6 +75,11 @@ Twinkle::Twinkle(): Music("TwinkleTwinkleLittleStar", 2, 60, 102) {
 
 Ocean::Ocean(): Music("我们都拥有海洋", 5, 70, 300) {
 
+    /**********************/
+    /**** 我们都拥有海洋 ****/
+    /**********************/
+    // adapted by: Calvin Cao
+
     /**** Type ****/
     this->setType(0, Acoustic_Grand_Piano);  // Low
     this->setType(1, Acoustic_Grand_Piano);  // Middle
@@ -84,9 +89,9 @@ Ocean::Ocean(): Music("我们都拥有海洋", 5, 70, 300) {
     /**** Additional Strength ****/
     this->setAdditionalStrength(2, 2);
 
-    /**** Music ****/
-    // part 1
+    /**** Music Start! ****/
 
+    // part A
     this->addNote(0, 0, 4, D3S, A3S, D4S);
     this->addNote(1, 0, 5, F4S, A4S);
 
@@ -124,17 +129,83 @@ Ocean::Ocean(): Music("我们都拥有海洋", 5, 70, 300) {
     this->addNote(2, 66, 5, D4S);
     this->addNote(2, 76, 8, F4S);
     this->addNote(2, 77, 8, C5S);
-    this->addNote(2, 78, 7, F4S);
-    this->addNote(2, 79, 8, F4S);
-    this->addNote(2, 82, 7, G4S);
-    this->addNote(2, 83, 8, A4S);
-    this->addNote(2, 86, 6, F4S);
-    this->addNote(2, 88, 5, F4);
+    this->addNote(2, 78, 9, F4S);
+    this->addNote(2, 79, 10, F4S);
+    this->addNote(2, 82, 6, G4S);
+    this->addNote(2, 83, 7, A4S);
+    this->addNote(2, 86, 4, F4S);
+    this->addNote(2, 88, 3, F4);
 
     // 无关那些催人启程的情节 这世界太吵 不代表你的声音不珍贵
     this->RepeatSingleChannal(2, 32, 96, 96);
     this->addNote(2, 64 + 90, 6, F4S);
     this->addNote(2, 64 + 91, 7, G4S);
 
-    int _BREAKPOINT0 = 64 + 96;
+    // Part B
+    int b_start = 64 + 96;
+    this->addNote(0, b_start, 10, B1);
+    this->addNote(1, b_start, 6, B2);
+    this->addNote(1, b_start + 2, 6, F3S);
+    this->addNote(1, b_start + 4, 6, B3);
+
+    this->addNote(0, b_start, 8, C2S);
+    this->addNote(1, b_start + 8, 6, C3S);
+    this->addNote(1, b_start + 10, 6, G3S);
+    this->addNote(1, b_start + 12, 6, C4S);
+
+    this->RepeatAllChannals(b_start, b_start + 16, b_start + 16);
+    this->RepeatAllChannals(b_start, b_start + 16, b_start + 32);
+
+    this->addNote(0, b_start + 48, 6, A2S, F3, A3S);
+    this->addNote(0, b_start + 56, 6, D3S, A3S, D4S);
+    this->addNote(1, b_start + 59, 4, F4);
+    this->addNote(1, b_start + 61, 4, F4S);
+
+    // 去哪  --让我选择
+    this->addNote(2, b_start - 2, 9, C5S);
+    this->addNote(2, b_start, 8, D4S);
+    this->addNote(3, b_start + 2, 6, D5S);
+    this->addNote(3, b_start + 3, 6, A5S);
+    this->addNote(3, b_start + 6, 6, D5S);
+    this->addNote(3, b_start + 7, 6, G5S);
+
+    // 如果  --只有岸没有海
+    this->RepeatSingleChannal(2, b_start - 2, b_start + 14, b_start + 14);
+    this->RepeatSingleChannal(2, b_start - 2, b_start + 14, b_start + 26);
+    this->addNote(3, b_start + 18, 6, D5S);
+    this->addNote(3, b_start + 19, 6, A5S);
+    this->addNote(3, b_start + 22, 7, C6S);
+    this->addNote(3, b_start + 23, 5, G5S);
+    this->addNote(3, b_start + 26, 6, A5S);
+    this->addNote(3, b_start + 27, 6, G5S);
+
+    // 那我  --长奔跑的脚踝
+    this->addNote(3, b_start + 34, 6, D5S);
+    this->addNote(3, b_start + 35, 7, A5S);
+    this->addNote(3, b_start + 38, 6, D5S);
+    this->addNote(3, b_start + 40, 8, G5S);
+    this->addNote(3, b_start + 42, 7, F5S);
+    this->addNote(3, b_start + 43, 6, F5);
+
+    // 到底是为什么
+    this->addNote(3, b_start + 45, 6, F5);
+    this->addNote(3, b_start + 46, 7, F5S);
+    this->addNote(3, b_start + 47, 9, G5S);
+    this->addNote(3, b_start + 48, 11, G5S);
+    this->CopyNote(3, b_start + 45, b_start + 49, 2, b_start + 45);
+    this->changeTune(2, b_start + 45, b_start + 49, -12);
+    this->addNote(3, b_start + 54, 11, D5S, G5);
+    this->addNote(2, b_start + 54, 8, A4S, D5S);
+    this->addNote(3, b_start + 55, 12, G5, A5S);
+    this->addNote(2, b_start + 55, 8, A4S, D5S);
+
+    this->addNote(0, b_start + 63, new stop_spo());
+    this->addNote(1, b_start + 63, new stop_spo());
+    this->addNote(2, b_start + 63, new stop_spo());
+    this->addNote(3, b_start + 63, new stop_spo());
+
+    // Part C
+    int c_start = 64 + 96 + 64;
+    this->addNote()
+
 }
